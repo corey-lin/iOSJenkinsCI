370394f9a7d26ae1763c2aba129e273e1042bd2e
Jenkins <jenkins@ITSG000418-MAC.local>
2017-04-25 18:50:06 +0800

    show red screen

--- iOSJenkinsCI/ViewController.swift
+++ iOSJenkinsCI/ViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import RxCocoa
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red
+        let button = UIButton(frame: CGRect(x: 50, y: 50, width: 100, height: 20))
+        self.view.addSubview(button)
+        button.rx.tap.subscribe { _ in
+            debugPrint("fjsldjfskdf")
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
