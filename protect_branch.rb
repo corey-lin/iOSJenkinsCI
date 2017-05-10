25f28b1866fc9dc0a0e06bd70c0b508bc1b3c92f
Corey Lin <lyfone@gmail.com>
2017-05-11 01:14:16 +0800

    update protect branch ruby script

--- protect_branch.rb
+++ protect_branch.rb
@@ -2,18 +2,18 @@
 
 require 'Octokit'
 
-client = Octokit::Client.new(:access_token => "b64c5c029dba728128dbc0b6d9a9e0382644cac9")
+if ARGV.length != 1 
+  puts "Usage: #{$PROGRAM_NAME} github_access_token"
+  exit
+end
 
-#branches = client.branches('corey-lin/iOSJenkinsCI', :accept => 'application/vnd.github.loki-preview+json')
+client = Octokit::Client.new(:access_token => ARGV[0])
+
 branches = client.branches('corey-lin/iOSJenkinsCI')
-puts branches.inspect
 branches.each do |b|
   puts b.name unless b.protected
   unless b.protected
     client.protect_branch('corey-lin/iOSJenkinsCI', b.name, required_status_checks: { :strict => false, :contexts => ['danger/danger'] }, :enforce_admins => false)
-    response = Octokit.last_response
-    puts response.inspect
-    break
   end
 end
 
